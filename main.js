cf8949fe2c0255ce97d4004d386f02745bba4cca
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-05-24 20:20:44 +0900

    complete

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ const img = mainContainer.querySelector("img");
 const dogStatus = mainContainer.querySelector("h6");
 
 let dogName = "";
+let count = 0;
 
 const handleGreeting = (e) => {
   e.preventDefault();
   if (input.value === null || input.value === "") {
     return alert("Please type dog's name");
   }
-  h6.innerText = `I'm ${input.value}`;
+  document.querySelector("h4").innerText = "";
+  h6.innerText = `Hi~ I'm ${input.value}!`;
   input.style.display = "none";
   actionBtn.innerText = "Start!";
   dogName = input.value;
@@ -30,10 +32,43 @@ const paintingDogBreed = (e) => {
 };
 const handleDogBreed = (e) => {
   const btnTarget = e.target;
+  console.log(count);
   if (btnTarget.innerText === "Feed") {
     img.src = "img/feed.png";
-    h6.innerText = `${dogName} love bread!`;
-    btnTarget.classList.add("btnClicked");
+    dogStatus.innerText = `${dogName} love bread!`;
+    btnTarget.className = "btnClicked";
+    btns[1].className = "btnNotClicked";
+    btns[2].className = "btnNotClicked";
+    btns[3].className = "btnNotClicked";
+    count++;
+  } else if (btnTarget.innerText === "Bath") {
+    img.src = "img/bath.jpg";
+    dogStatus.innerText = `${dogName} hate water...`;
+    btnTarget.className = "btnClicked";
+    btns[0].className = "btnNotClicked";
+    btns[2].className = "btnNotClicked";
+    btns[3].className = "btnNotClicked";
+    count++;
+  } else if (btnTarget.innerText === "Pat") {
+    img.src = "img/pat.png";
+    dogStatus.innerText = `${dogName} love you brooo~~`;
+    btnTarget.className = "btnClicked";
+    btns[0].className = "btnNotClicked";
+    btns[1].className = "btnNotClicked";
+    btns[3].className = "btnNotClicked";
+    count++;
+  } else if (btnTarget.innerText === "Walk") {
+    img.src = "img/walk.png";
+    dogStatus.innerText = `Yoo-hoo~~ ${dogName} happy!`;
+    btnTarget.className = "btnClicked";
+    btns[0].className = "btnNotClicked";
+    btns[1].className = "btnNotClicked";
+    btns[2].className = "btnNotClicked";
+    count++;
+  }
+  if (count >= 20) {
+    img.src = "img/stop.png";
+    dogStatus.innerText = `${dogName} is too tired...`;
   }
 };
 
